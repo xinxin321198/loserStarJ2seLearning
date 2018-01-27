0f16a11d04cedb42ef32c0a426fe699e3fa7f2c8
xinxin321198 <362527240@qq.com>
2018-01-28 00:49:30 +0800

    运算符

---
+++ src/main/java/com/loserstar/operator/Operator.java
@@ -0,0 +1,31 @@
+package com.loserstar.operator;
+
+/**
+ * 运算符
+ * author: loserStar
+ * date: 2018年1月28日上午12:36:25
+ * email:362527240@qq.com
+ * github:https://github.com/xinxin321198
+ * remarks:
+ */
+public class Operator {
+
+	public static void main(String[] args) {
+		//位运算
+		/*  ~取反
+		 *  & 按位与
+		 *  |按位或
+		 *  ^按位异或
+		 *  << 左移运算符
+		 *  >>友谊运算符
+		 *  
+		 *  右移一位相当于除2取商
+		 *  左移一位相当于乘2
+		 */
+		int a = 3*2*2 ;
+		int b = 3<<2 ;
+		int c = 12/2/2 ;
+		int d = 12>>2;
+	}
+
+}
